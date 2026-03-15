18e355aab20bb60ada5337fba62316f0f8bb9f91
binduanisetti1701-source <binduanisetti1701@gmail.com>
2026-03-15 17:56:40 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+
+class Submission(models.Model):
+    student_name = models.CharField(max_length=100)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
